5c3ccb65c1d92d9c02c0a6e6ed8439ec1661c6b2
Przemek587 <72383546+Przemek587@users.noreply.github.com>
2022-08-16 10:39:25 +0200

    add style to message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,8 +4,8 @@ import { auth } from '../firebase'
 const style = {
     message: `flex items-center shadow-xl m-4 px3 py-2 rounded-tl-full rounded-tr-full`,
     name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-    sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-    received: `bg-[#e5e5ea] text-black float-left rounded-br-full`
+    sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full p-4`,
+    received: `bg-[#e5e5ea] text-black float-left rounded-br-full p-4`
 }
 
 
